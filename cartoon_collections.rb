1acc8d5d3395f00a93128338bc355eb352cf27f2
Aaron Berns <anberns@gmail.com>
2019-01-08 19:27:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,44 @@ def long_planeteer_calls(array)
   
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    return cheese if array.include?(cheese)
+  end
+  nil
+    
 end
+#### Method 4 — `find_the_cheese`
+
+![dancing-mice](https://s3-us-west-2.amazonaws.com/web-dev-readme-photos/cartoon-collections/cheese.jpg)
+
+The `find_the_cheese` method should accept an array of strings. It should then
+look through these strings to find and return the first string that is a type of
+cheese. The types of cheese that appear are `"cheddar"`, `"gouda"`, and
+`"camembert"`.
+
+For example:
+
+```ruby
+snacks = ["crackers", "gouda", "thyme"]
+find_the_cheese(snacks)
+#=> "gouda"
+
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(soup)
+#=> "cheddar"
+```
+
+If, sadly, a list of ingredients does not include cheese, return `nil`:
+
+```ruby
+ingredients = ["garlic", "rosemary", "bread"]
+find_the_cheese(ingredients)
+#=> nil
+```
+
+You can assume that all strings will be lowercase. Take a look at the
+[`.include`][include] method for a hint. This method asks you to return a string
+value instead of printing it so keep that in mind.
