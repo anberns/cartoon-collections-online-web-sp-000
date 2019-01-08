95f9d9af344fc5610392b260a72bc72e472294fa
Aaron Berns <anberns@gmail.com>
2019-01-08 19:31:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,41 +31,10 @@ end
 def find_the_cheese(array)
   
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.each do |cheese|
-    return cheese if array.include?(cheese)
+  array.each do |value|
+    cheese_types.each do |cheese|
+      return true cheese if value == cheese
   end
   nil
     
 end
-#### Method 4 — `find_the_cheese`
-
-![dancing-mice](https://s3-us-west-2.amazonaws.com/web-dev-readme-photos/cartoon-collections/cheese.jpg)
-
-The `find_the_cheese` method should accept an array of strings. It should then
-look through these strings to find and return the first string that is a type of
-cheese. The types of cheese that appear are `"cheddar"`, `"gouda"`, and
-`"camembert"`.
-
-For example:
-
-```ruby
-snacks = ["crackers", "gouda", "thyme"]
-find_the_cheese(snacks)
-#=> "gouda"
-
-soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
-find_the_cheese(soup)
-#=> "cheddar"
-```
-
-If, sadly, a list of ingredients does not include cheese, return `nil`:
-
-```ruby
-ingredients = ["garlic", "rosemary", "bread"]
-find_the_cheese(ingredients)
-#=> nil
-```
-
-You can assume that all strings will be lowercase. Take a look at the
-[`.include`][include] method for a hint. This method asks you to return a string
-value instead of printing it so keep that in mind.
